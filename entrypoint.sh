bd12a42d3e3364ee588fb229583d9f475af04c14
okubanjoe <72366009+okubanjoe@users.noreply.github.com>
2023-08-08 17:45:49 -0400

    First Push

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@
 # Set the initial value of the counter
 counter=1
 
-while [ "$counter" -le 10 ]; do
+while [ ${counter} -le 10 ]; do
 
   # Print the value of the counter
-  echo "$counter"
+  echo "${counter}"
 
   # Increment the counter
   counter=$((counter + 1))
