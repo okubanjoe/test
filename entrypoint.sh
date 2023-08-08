8372481bf57750dad9e3d2527551518150c80a10
okubanjoe <72366009+okubanjoe@users.noreply.github.com>
2023-08-08 17:17:16 -0400

    First Push

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
-#!/bin/sh -l
+#!/bin/bash
 
-echo "Hello $1"
+# Get the first argument from the command line
+name=$1
+
+# Print a greeting to the user
+echo "Hello $name"
+
+# Get the current time
 time=$(date)
+
+# Append the current time to the GitHub output file
 echo "time=$time" >> $GITHUB_OUTPUT
